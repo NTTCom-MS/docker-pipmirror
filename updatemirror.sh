450f24a0aaa9173d24e5c7d923ccb1a7615dc193
Jordi Prats <jordi.prats@gmail.com>
2019-06-18 12:32:21 +0200

    /usr/local/bin/pypi-mirror

--- updatemirror.sh
+++ updatemirror.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3.6 /usr/bin/pypi-mirror -d /var/lib/pipmirror/packages download nagiosplugin
-python3.6 /usr/bin/pypi-mirror -d /var/lib/pipmirror/packages create -m /var/lib/pipmirror/webroot
+/usr/local/bin/pypi-mirror -d /var/lib/pipmirror/packages download nagiosplugin
+/usr/local/bin/pypi-mirror -d /var/lib/pipmirror/packages create -m /var/lib/pipmirror/webroot
